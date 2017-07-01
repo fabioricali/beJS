fb9f2b56558216b6f6a46d0fe96f47d72a143853
Fabio Ricali <ricalifabio@gmail.com>
2017-07-01 09:35:33 +0200

    update script

--- dist/be.js
+++ dist/be.js
@@ -1,4 +1,4 @@
-// [AIV]  beJS Build version: 1.0.9  
+// [AIV]  beJS Build version: 1.0.10  
  var be =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -870,7 +870,7 @@ module.exports = __webpack_require__(5);
 
 var Helpers = __webpack_require__(2);
 var Interface = __webpack_require__(0);
-var version = '1.0.9';
+var version = '1.0.10';
 
 /**
  * be class
--- version-to-tag.sh
+++ version-to-tag.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
-git tag "v$PACKAGE_VERSION"
+git tag -a "v$PACKAGE_VERSION" -m "v$PACKAGE_VERSION" && git push &&  git push --tags
