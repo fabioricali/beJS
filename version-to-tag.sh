9447dba429a4a8ff4dc71488427fbd23ab32bc01
Fabio Ricali <ricalifabio@gmail.com>
2017-07-01 09:48:33 +0200

    update script bash

--- version-to-tag.sh
+++ version-to-tag.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
+git commit -m "build v$PACKAGE_VERSION"
 git tag -a "v$PACKAGE_VERSION" -m "v$PACKAGE_VERSION"
 git push &&  git push --tags
