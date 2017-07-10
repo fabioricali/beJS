ca8da8e155f3d03cfae761f96ba83f0a09c9a57a
fabioricali <ricalifabio@gmail.com>
2017-07-10 20:38:53 +0200

    v1.5.0

--- dist/be.js
+++ dist/be.js
@@ -1,4 +1,4 @@
-// [AIV]  beJS Build version: 1.4.1  
+// [AIV]  beJS Build version: 1.5.0  
  var be =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -1386,7 +1386,7 @@ module.exports = __webpack_require__(5);
 
 var Helpers = __webpack_require__(2);
 var Interface = __webpack_require__(0);
-var version = '1.4.1';
+var version = '1.5.0';
 
 /**
  * be class
@@ -5714,7 +5714,7 @@ module.exports = DOM;
 
 module.exports = {
 	"name": "bejs",
-	"version": "1.4.1",
+	"version": "1.5.0",
 	"description": "Simple, light-weight assertions framework for javascript",
 	"homepage": "https://be.js.org",
 	"main": "index.js",
