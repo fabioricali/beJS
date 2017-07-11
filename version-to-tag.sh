6031216bbeaf7e22e1d6b5f61124b999fab57f50
Fabio Ricali <ricalifabio@gmail.com>
2017-07-11 20:52:32 +0200

    remove doc

--- version-to-tag.sh
+++ version-to-tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
-git add docs/bejs/"$PACKAGE_VERSION"
+#git add docs/bejs/"$PACKAGE_VERSION"
 git commit -a -m "v$PACKAGE_VERSION"
 git tag -a "v$PACKAGE_VERSION" -m "v$PACKAGE_VERSION"
 git push &&  git push --tags
