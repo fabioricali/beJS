9fb1ba1b4dbb01c9020ac935df131b3ee90d953a
Fabio Ricali <ricalifabio@gmail.com>
2017-07-20 23:06:36 +0200

    v1.6.3

--- dist/be.js
+++ dist/be.js
@@ -1,4 +1,4 @@
-// [AIV]  beJS Build version: 1.6.2  
+// [AIV]  beJS Build version: 1.6.3  
  var be =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -1696,7 +1696,7 @@ module.exports = __webpack_require__(6);
 
 var Helpers = __webpack_require__(2);
 var Interface = __webpack_require__(0);
-var version = '1.6.2';
+var version = '1.6.3';
 
 /**
  * be class
@@ -5927,7 +5927,7 @@ module.exports = DOM;
 
 module.exports = {
 	"name": "bejs",
-	"version": "1.6.2",
+	"version": "1.6.3",
 	"description": "Simple, light-weight assertions framework for javascript",
 	"homepage": "https://be.js.org",
 	"main": "index.js",
